ce3d372a11a64eab6582012ec0be1539fa14ee01
Thomas BENARD <thomas0benard@gmail.com>
2021-11-07 08:57:59 +0100

    fixed react router blank page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-import "./pages/homepage/homepage.styles.scss";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
